5785562636af05186f18fc6a20868980c0ef8acb
Praveen <111781869+pscode22@users.noreply.github.com>
2023-07-04 00:11:23 +0530

    add search functionality to fav section.

--- src/routes/favorites.tsx
+++ src/routes/favorites.tsx
@@ -1,17 +1,44 @@
 import ExerciseCard from '@/components/common/exercise-card';
 import { CtxFavoriteExercises } from '@/context/favorites.ctx';
-import { Container, Grid, Text } from '@mantine/core';
-import { useContext } from 'react';
+import { Exercise } from '@/interfaces';
+import { Box, Container, Flex, Grid, Input } from '@mantine/core';
+import { IconSearch } from '@tabler/icons-react';
+import { useContext, useState } from 'react';
 
 export default function RouteFavorites() {
   const { favoriteExercises } = useContext(CtxFavoriteExercises);
 
+  const [searchedValue, setSearchedValue] = useState<string>('');
+  const [filteredBySearch, setFilteredBySearch] = useState<Exercise[]>(favoriteExercises);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchedValue(e.target.value);
+    const query = e.target.value.toUpperCase();
+    let getData = [...favoriteExercises];
+
+    getData = getData.filter(item => {
+      return item.name.toUpperCase().indexOf(query) !== -1;
+    });
+    setFilteredBySearch([...getData]);
+  };
+
   return (
     <div>
-      <Text>Favorites Section</Text>
+      {/* <Text>Favorites Section</Text> */}
       <Container fluid>
+        <Flex justify="center" align="center" mt="md" mb="md">
+          <Box w={{ base: 200, sm: 400, lg: 500 }}>
+            <Input
+              icon={<IconSearch />}
+              placeholder="Search by name"
+              value={searchedValue}
+              onChange={e => handleChange(e)}
+              bg="#25262b"
+            />
+          </Box>
+        </Flex>
         <Grid px="xl" gutter="xl">
-          {favoriteExercises.map(item => {
+          {filteredBySearch.map(item => {
             return (
               <Grid.Col
                 lg={3}
